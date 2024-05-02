d364c6f7faaedbf33b97e28ddcb5932a730a3a2e
Zablon Samba <131341104+Kakazablone@users.noreply.github.com>
2024-05-02 15:29:48 +0300

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type annoted function concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return concatenated strings
+    """
+    return str1 + str2
