06edb4967719e263103d73bdbcf3871a09912c6c
Zablon Samba <131341104+Kakazablone@users.noreply.github.com>
2024-05-03 17:10:02 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,8 +3,11 @@
 Type-annotated function safe_first_element
 """
 
-from typing import Any, Optional
+from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: list) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Return the first element of a sequence
+    """
     return lst[0] if lst else None
